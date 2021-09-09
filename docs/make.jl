5227645f71c86837e5bc9c523b2e3ab8350c1aec
Ran Gal <rgal@microsoft.com>
2021-09-09 09:44:58 -0700

    added basic test and docs folder - will start filling them up asapa

---
+++ docs/make.jl
@@ -0,0 +1,22 @@
+
+using Documenter
+using Twinkle, Twinkle.FlexUI
+
+makedocs(
+    sitename = "Twinkle.jl",
+    format = Documenter.HTML(
+        # prettyurls = get(ENV, "CI", nothing) == "true",
+        assets = [asset("assets/blink.svg", class = :ico, islocal = true)],
+    ),
+    modules = [Twinkle],
+    pages = [
+        "Home" => "index.md",
+    ],
+    expandfirst = [])
+
+# deploydocs(
+#     repo = "github.com/microsoft/OpticSim.jl.git",
+#     devbranch = "main",
+#     push_preview = true,
+# )
+
